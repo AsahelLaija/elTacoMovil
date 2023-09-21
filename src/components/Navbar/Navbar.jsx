2373b48d4d7451b1a35b2a57fad78b4b0865d5fe
Asahel Laija <asahellaija@gmail.com>
2023-09-21 17:10:24 -0500

    bun try

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { MdOutlineRestaurantMenu } from 'react-icons/md';
 import images from '../../constants/images';
 import './Navbar.css';
+
+const svgStyle = {
+    color: "rgb(255, 255, 255)"
+};
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = React.useState(false);
@@ -26,10 +28,27 @@ const Navbar = () => {
         <a href="/" className="p__opensans">Book Table</a>
       </div>
       <div className="app__navbar-smallscreen">
-        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
+        <svg 
+            onClick={() => setToggleMenu(true)}
+            stroke="currentColor" 
+            fill="currentColor" 
+            stroke-width="0" 
+            viewBox="0 0 512 512" 
+            color="#fff" 
+            font-size="27" 
+            height="1em" 
+            width="1em" 
+            xmlns="http://www.w3.org/2000/svg" 
+            style={ svgStyle }>
+            <path d="M32 96v64h448V96H32zm0 128v64h448v-64H32zm0 128v64h448v-64H32z"></path>
+        </svg>
         {toggleMenu && (
           <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
-            <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
+            <svg 
+                onClick={() => setToggleMenu(false)}
+                stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" font-size="27" class="overlay__close" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M8.1 13.34l2.83-2.83L3.91 3.5a4.008 4.008 0 000 5.66l4.19 4.18zm6.78-1.81c1.53.71 3.68.21 5.27-1.38 1.91-1.91 2.28-4.65.81-6.12-1.46-1.46-4.2-1.1-6.12.81-1.59 1.59-2.09 3.74-1.38 5.27L3.7 19.87l1.41 1.41L12 14.41l6.88 6.88 1.41-1.41L13.41 13l1.47-1.47z"></path></svg>
+
+
             <ul className="app__navbar-smallscreen_links">
               <li><a href="#home" onClick={() => setToggleMenu(false)}>Home</a></li>
               <li><a href="#about" onClick={() => setToggleMenu(false)}>About</a></li>
--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
-
 import { meal } from '../../constants';
 import './Intro.css';
+
+const svgStyle = {
+    color: "rgb(255, 255, 255)"
+}
 
 const Intro = () => {
   const [playVideo, setPlayVideo] = React.useState(false);
@@ -31,9 +33,33 @@ const Intro = () => {
           }}
         >
           {playVideo ? (
+
+
+              <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" color="#fff" font-size={30} height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" style={svgStyle}><path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z"></path></svg>
+
+              /*
             <BsPauseFill color="#fff" fontSize={30} />
+              */
+
           ) : (
+
+              /*
             <BsFillPlayFill color="#fff" fontSize={30} />
+              */
+
+            <svg 
+              stroke="currentColor" 
+              fill="currentColor" 
+              stroke-width="0" 
+              viewBox="0 0 16 16" 
+              color="#fff" 
+              font-size={30}
+              height="1em" 
+              width="1em" 
+              xmlns="http://www.w3.org/2000/svg" 
+              style={svgStyle}>
+              <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"></path>
+            </svg>
           )}
         </div>
       </div>
